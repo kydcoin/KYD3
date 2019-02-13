be25e2f96ab054c7f44f45fccebb25870ce7e9d0
enginama <enginama@pjproductions.net>
2019-02-13 12:32:42 -0600

    Update kyd-install.sh

--- contrib/masternodes/kyd-install.sh
+++ contrib/masternodes/kyd-install.sh
@@ -373,7 +373,9 @@ cp /etc/systemd/system/KYD.service /etc/systemd/system/kyd.service > /dev/null 2
 systemctl stop $COIN_NAME.service > /dev/null 2>&1
 systemctl stop KYD.service > /dev/null 2>&1
 rm /etc/systemd/system/KYD.service > /dev/null 2>&1
+echo -e "Downloading updated daemon files"
 download_node
+echo -e "Restarting Node"
   systemctl start $COIN_NAME.service
   echo -e "${GREEN}$COIN_NAME Masternode has been updated!${NC}"
 exit 1
