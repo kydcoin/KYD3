7c16e09ba24dd2d5fd91021ae30119fbdf1c5c3a
enginama <enginama@pjproductions.net>
2019-06-25 11:17:59 -0500

    corrections to urls

--- contrib/masternodes/kyd-install.sh
+++ contrib/masternodes/kyd-install.sh
@@ -10,10 +10,10 @@ COIN_DAEMON='kydd'
 COIN_CLI='kyd-cli'
 COIN_PATH='/usr/local/bin/'
 if [[ $(lsb_release -d) = *16.04* ]]; then
-COIN_TGZ='https://github.com/kydcoin/KYD3/releases/download/v3.2.1/kyd-3.2.1.0-Ubuntu16-x86_64.tar.gz'
+COIN_TGZ='https://github.com/kydcoin/KYD3/releases/download/3.2.1/kyd-3.2.1.0-Ubuntu16-x86_64.1.tar.gz'
 fi
 if [[ $(lsb_release -d) = *18.04* ]]; then
-COIN_TGZ='https://github.com/kydcoin/KYD3/releases/download/v3.2.1/kyd-3.2.1.0-Ubuntu18-x86_64.tar.gz'
+COIN_TGZ='https://github.com/kydcoin/KYD3/releases/download/3.2.1/kyd-3.2.1.0-Ubuntu18-x86_64.tar.gz'
 fi
 COIN_BOOTSTRAP='https://review.kydcoin.io/bootstrap/kyd-bootstrap.zip'
 BOOTSTRAP_ZIP=$(echo $COIN_BOOTSTRAP | awk -F'/' '{print $NF}')
