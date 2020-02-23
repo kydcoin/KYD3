0b5073bfdf16146e0a11f921f902e14383693240
enginama <enginama@pjproductions.net>
2020-02-23 07:54:52 -0600

    Update kyd-install.sh

--- contrib/masternodes/kyd-install.sh
+++ contrib/masternodes/kyd-install.sh
@@ -185,8 +185,8 @@ fi
 
 function prepare_system() {
 echo -e "Preparing the system to install ${GREEN}$COIN_NAME${NC} masternode."
-apt-get update
-apt-get upgrade
+apt-get update -y
+apt-get upgrade -y
 #DEBIAN_FRONTEND=noninteractive apt-get update > /dev/null 2>&1
 #DEBIAN_FRONTEND=noninteractive apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" -y -qq upgrade >/dev/null 2>&1
 apt install -y software-properties-common >/dev/null 2>&1
@@ -204,7 +204,7 @@ if [ "$?" -gt "0" ];
     echo "apt-get update"
     echo "apt -y install software-properties-common"
     echo "apt-add-repository -y ppa:bitcoin/bitcoin"
-    echo "apt-get update"
+    echo "apt-get upgrade"
     echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
 libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git curl libdb4.8-dev \
 bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw fail2ban pkg-config libevent-dev libzmq5"
