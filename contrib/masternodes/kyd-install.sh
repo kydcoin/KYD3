bcb86d6ec262e6931da920644aa06bb05e0e40f6
enginama <enginama@pjproductions.net>
2020-06-20 10:13:50 -0500

    Update kyd-install.sh

--- contrib/masternodes/kyd-install.sh
+++ contrib/masternodes/kyd-install.sh
@@ -202,8 +202,8 @@ if [ "$?" -gt "0" ];
     echo "apt-add-repository -y ppa:bitcoin/bitcoin"
     echo "apt-get upgrade"
     echo "apt install -y make build-essential libtool software-properties-common autoconf libssl-dev libboost-dev libboost-chrono-dev libboost-filesystem-dev \
-libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git curl libdb4.8-dev \
-bsdmainutils libdb4.8++-dev libminiupnpc-dev libgmp3-dev ufw fail2ban pkg-config libevent-dev libzmq5 libdb4.8-dev libdb4.8++-dev"
+libboost-program-options-dev libboost-system-dev libboost-test-dev libboost-thread-dev sudo automake git curl \
+bsdmainutils libminiupnpc-dev libgmp3-dev ufw fail2ban pkg-config libevent-dev libzmq5"
  exit 1
 fi
 
